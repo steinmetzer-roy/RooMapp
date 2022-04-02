9218d38717685fba4b5e84da3835940608e33274
Jim <020167214B@uni.lu>
2022-04-02 22:31:53 +0200

    Added image to View.

--- RooMapp/src/Screens/MapScreen.js
+++ RooMapp/src/Screens/MapScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {Image} from "react-native-web";
 
 const MapScreen = () => {
 
     return (
         <View>
-
+            <Image source={require('@../../assets/map-floor3.png')}/>
         </View>
     )
 }

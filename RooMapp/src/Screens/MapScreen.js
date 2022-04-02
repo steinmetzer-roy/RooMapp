f551760be08d0f86e438520f62f320ca791d6e7b
Jim <020167214B@uni.lu>
2022-04-02 22:44:11 +0200

    Cleaned up app.js

--- RooMapp/src/Screens/MapScreen.js
+++ RooMapp/src/Screens/MapScreen.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import {Image} from "react-native-web";
+import { View, Text, StyleSheet,Image } from 'react-native';
 
 const MapScreen = () => {
 
     return (
         <View>
-            <Image source={require('@../../assets/map-floor3.png')}/>
+            <Image style={styles.image}
+                   source={require('../../assets/map-floor3.png')}/>
+            <Text>
+                Helloaa
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image : {
+      width: 500,
+      height:500,
+    },
+
+});
 
 export default MapScreen;

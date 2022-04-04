a922515ab1c1648916d0d7a3040e8f1d20de8435
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-04-04 12:33:50 +0200

    Implement Navigation between screens

--- RooMapp/App.js
+++ RooMapp/App.js
@@ -1,4 +1,7 @@
 import CourseScreen from './src/Screens/CourseScreen';
+import SideBar from './src/Screens/SideBarScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
 import {
     Keyboard,
@@ -11,8 +14,25 @@ import {
     View
 } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
     return (
-        <CourseScreen></CourseScreen>
+        <NavigationContainer>
+            <Stack.Navigator>
+            {/* Add screen as followed to the top of all Stack.Screen to have it shown first: 
+            <Stack.Screen name="YOURSCREENNAME" component={SCREENCOMPONENTNAME} options={{ headerShown:1}}/>
+            
+            -name: freely choosable
+            -component: name of the component
+            -headerShown: to disable header insert 0
+            
+            */ }
+            
+            <Stack.Screen name="CourseScreen" component={CourseScreen} options={{ headerShown:1}}/>
+            <Stack.Screen name="SideBar" component={SideBar} options={{ headerShown:1}}/>
+                
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }

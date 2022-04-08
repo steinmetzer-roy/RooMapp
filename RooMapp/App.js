0399acad5540039fafd5fd67c30694198efcafe8
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-04-08 11:08:00 +0200

    Implement Sidebar(Drawer) & Remove Stack

--- RooMapp/App.js
+++ RooMapp/App.js
@@ -1,7 +1,10 @@
+//Custom Screens
 import CourseScreen from './src/Screens/CourseScreen';
-import SideBar from './src/Screens/SideBarScreen';
+import MapScreen from './src/Screens/MapScreen';
+//Dependencies for Sidebar and Screenhandling
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+//Default React Components
 import React, { useState } from 'react';
 import {
     Keyboard,
@@ -14,25 +17,15 @@ import {
     View
 } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-            {/* Add screen as followed to the top of all Stack.Screen to have it shown first: 
-            <Stack.Screen name="YOURSCREENNAME" component={SCREENCOMPONENTNAME} options={{ headerShown:1}}/>
-            
-            -name: freely choosable
-            -component: name of the component
-            -headerShown: to disable header insert 0
-            
-            */ }
-            
-            <Stack.Screen name="CourseScreen" component={CourseScreen} options={{ headerShown:1}}/>
-            <Stack.Screen name="SideBar" component={SideBar} options={{ headerShown:1}}/>
-                
-            </Stack.Navigator>
+            <Drawer.Navigator useLegacyImplementation>
+                <Drawer.Screen name="CourseScreen" component={CourseScreen} />
+                <Drawer.Screen name="MapScreen" component={MapScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }

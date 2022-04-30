400a657d52264499d2b5665be1dbd1d28a140bb0
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-04-30 18:52:40 +0200

    Updated style

--- RooMapp/App.js
+++ RooMapp/App.js
@@ -3,32 +3,45 @@ import CourseScreen from './src/Screens/CourseScreen';
 import MapScreen from './src/Screens/MapScreen';
 import SwipeGesture from './src/components/SwipeGesture';
 //Dependencies for Sidebar and Screenhandling
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 //Default React Components
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Text, View } from 'react-native';
 //Module
 import { styles, stylesDarkMode } from './src/module/Styles';
+import { TouchableOpacity } from 'react-native-web';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
 
     const [customStyle, setcustomStyle] = useState(stylesDarkMode)
-    
+
     const DrawerEl = props =>
         <>
-            <Button title='CourseScreen' onPress={() => { props.navigation.navigate('CourseScreen') }}></Button>
-            <Button title='MapScreen' onPress={() => { props.navigation.navigate('MapScreen') }}></Button>
-            <Button title='DarkMode' onPress={() => { customStyle == styles ? setcustomStyle(stylesDarkMode) : setcustomStyle(styles)}}></Button>
+            <TouchableOpacity onPress={() => { props.navigation.navigate('CourseScreen') }}>
+                <View style={customStyle.drawerButtonBackgroundStyle}>
+                    <Text style={customStyle.drawerButtonTitleStyle}>CourseScreen</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { props.navigation.navigate('MapScreen') }}>
+                <View style={customStyle.drawerButtonBackgroundStyle}>
+                    <Text style={customStyle.drawerButtonTitleStyle}>MapScreen</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { customStyle == styles ? setcustomStyle(stylesDarkMode) : setcustomStyle(styles) }}>
+                <View style={customStyle.drawerButtonBackgroundStyle}>
+                    <Text style={customStyle.drawerButtonTitleStyle}>DarkMode</Text>
+                </View>
+            </TouchableOpacity>
         </>
 
     return (
-        <NavigationContainer>
-            <Drawer.Navigator useLegacyImplementation screenOptions={{ headerTransparent: true, headerTitle: "" }} drawerContent={props => DrawerEl(props)}>
-                <Drawer.Screen name="CourseScreen" children={({navigation}) => <SwipeGesture child={<CourseScreen customStyle={customStyle}></CourseScreen>} navigation={navigation}></SwipeGesture>} />
-                <Drawer.Screen name="MapScreen" children={({navigation,route}) => <SwipeGesture child={<MapScreen navigation={navigation} route={route}></MapScreen>} navigation={navigation}></SwipeGesture>} />
+        <NavigationContainer >
+            <Drawer.Navigator useLegacyImplementation screenOptions={{ headerTransparent: true, headerTitle: "", headerShown:false,drawerStyle:customStyle.drawerNavStyle }} drawerContent={props => DrawerEl(props)}>
+                <Drawer.Screen name="CourseScreen" children={({ navigation }) => <SwipeGesture child={<CourseScreen customStyle={customStyle}></CourseScreen>} navigation={navigation}></SwipeGesture>} />
+                <Drawer.Screen name="MapScreen" children={({ navigation, route }) => <SwipeGesture child={<MapScreen navigation={navigation} route={route}></MapScreen>} navigation={navigation}></SwipeGesture>} />
                 <Drawer.Screen name="DarkMode" component={() => { }} />
             </Drawer.Navigator>
         </NavigationContainer >
--- RooMapp/src/module/Styles.js
+++ RooMapp/src/module/Styles.js
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#EBEAED',
     },
     courseWrapper: {
-        padding: 80,
+        padding: 10,
         paddingHorizontal: 20,
         flex: 1,
     },
@@ -16,76 +16,6 @@ const styles = StyleSheet.create({
     },
     item: {
         backgroundColor: '#FFF',
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
-    items: {
-        marginTop: 30,
-    },
-    writeCourseWrapper: {
-        position: 'absolute',
-        bottom: 60,
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
-    input: {
-        paddingVertical: 15,
-        paddingHorizontal: 15,
-        backgroundColor: '#FFF',
-        borderRadius: 60,
-        borderColor: '#C0C0C0',
-        borderWidth: 1,
-        width: 250,
-    },
-    addWrapper: {
-        width: 60,
-        height: 60,
-        backgroundColor: '#FFF',
-        borderRadius: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderColor: '#C0C0C0',
-        borderWidth: 1,
-    },
-    itemLeft: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        flexWrap: 'wrap'
-    },
-    square:
-    {
-        width: 24,
-        height: 24,
-        backgroundColor: '#55BCF6',
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
-    },
-});
-
-const stylesDarkMode = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#555b65',
-    },
-    courseWrapper: {
-        padding: 80,
-        paddingHorizontal: 20,
-        flex: 1,
-        backgroundColor: 'red',
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    item: {
-        backgroundColor: 'blue',
         padding: 15,
         borderRadius: 10,
         flexDirection: 'row',
@@ -97,13 +27,12 @@ const stylesDarkMode = StyleSheet.create({
     {
         width: 24,
         height: 24,
-        backgroundColor: 'darkblue',
+        backgroundColor: '#55BCF6',
         opacity: 0.4,
         borderRadius: 5,
         marginRight: 15,
     },
     itemLeft: {
-        backgroundColor: 'green',
         flexDirection: 'row',
         alignItems: 'center',
         flexWrap: 'wrap'
@@ -111,25 +40,100 @@ const stylesDarkMode = StyleSheet.create({
     input: {
         paddingVertical: 15,
         paddingHorizontal: 15,
-        backgroundColor: 'pink',
+        backgroundColor: '#FFF',
         borderRadius: 60,
         borderColor: '#C0C0C0',
         borderWidth: 1,
         width: 250,
         marginTop: 10,
     },
-    addCourseButton: {
-        width: 55,
-        height: 55,
-        backgroundColor: '#FFF',
-        borderRadius: 60,
-        justifyContent: 'center',
+    drawerButtonBackgroundStyle:{
+        flex: 1,
+        paddingVertical: 15,
+        borderRadius: 30,
+        borderWidth: 1,
+        width: 250,
+        borderColor: '#C0C0C0'
+    },
+    drawerButtonTitleStyle:{
+        textAlign:'center',
+    },
+    drawerNavStyle:{
+        backgroundColor: 'white',
+        alignItems:'center',
+        width: 250,
+
+    }
+});
+
+const stylesDarkMode = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#2A2D32',
+    },
+    courseWrapper: {
+        padding: 10,
+        paddingHorizontal: 20,
+        flex: 1,
+        backgroundColor: '#2A2D32',
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#888C93'
+    },
+    item: {
+        backgroundColor: '#888C93',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 15,
+    },
+    square:
+    {
+        width: 24,
+        height: 24,
+        backgroundColor: 'black',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#2A2D32',
+        borderRadius: 60,
         borderColor: '#C0C0C0',
         borderWidth: 1,
-        textAlign: 'center',
-        textAlignVertical: 'auto',
-        fontSize: 22
+        width: 250,
+        marginTop: 10,
+        color: '#C0C0C0'
+    },
+    drawerButtonBackgroundStyle:{
+        flex: 1,
+        paddingVertical: 15,
+        borderRadius: 30,
+        borderWidth: 1,
+        width: 250,
+        backgroundColor: '#2A2D32',
+    },
+    drawerButtonTitleStyle:{
+        color: '#C0C0C0',
+        textAlign:'center'
+
+    },
+    drawerNavStyle:{
+        backgroundColor: '#C0C0C0',
+        alignItems:'center',
+        width: 250,
+
     }
 });
 

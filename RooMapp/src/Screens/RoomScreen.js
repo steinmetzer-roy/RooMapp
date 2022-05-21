7c07dcf13480e569063263123fc4c3884681b708
Jim <020167214B@uni.lu>
2022-05-21 18:34:52 +0200

    Finished doing the roomscreen.

--- RooMapp/src/Screens/RoomScreen.js
+++ RooMapp/src/Screens/RoomScreen.js
@@ -1,44 +1,60 @@
-import {Pressable, Text, View, Image, Dimensions} from "react-native";
+import {Pressable, Text, View, Image, Dimensions, StyleSheet} from "react-native";
 import React from "react";
 import {roomInfo} from "../components/data/RoomData";
 
 //these are the elements inside the modal
-const RoomScreen = ({ customStyle, modalRoom, setShowModal }) => {
+const RoomScreen = ({customStyle, modalRoom, setShowModal}) => {
 
     const dimensions = Dimensions.get('window');
-    const imageHeight = Math.round(dimensions.width * 2160 / 2880)*0.2;
-    const imageWidth = dimensions.width*0.2;
+    const imageHeight = Math.round(dimensions.width * 2160 / 2880) * 0.2;
+    const imageWidth = dimensions.width * 0.2;
 
-    let imagePath = require("../../assets/rooms/3210  1.jpg");
+    let imagePath;
+    try {
+        imagePath = require("../../assets/rooms/"+modalRoom.slice(0,1)+modalRoom.slice(2,5) +" 1.jpg");
+    } catch (e) {
+        console.log("Could not find image in " + "../../assets/rooms/"+modalRoom.slice(0,1)+modalRoom.slice(2,5) +" 1.jpg")
+        imagePath = "";
+    }
+
 
     return (<View style={customStyle.modalView}>
         <View style={customStyle.modal}>
             <Text style={customStyle.title}>
                 Room: {modalRoom}
-            </Text >
-            <View style={{flexDirection: "row", width: imageWidth*2}}>
-                <View style={{ margin: 15, justifyContent: "center"}}>
-                    <Text style={{fontSize: 18, justifyContent: "center"}}>
+            </Text>
+            <View style={{flexDirection: "row", width: imageWidth * 2}}>
+                <View style={styles.view}>
+                    <Text style={styles.text}>
                         Number of seats: {roomInfo.find((elements => elements.room === modalRoom)).seats}
                     </Text>
-                    <Text style={{fontSize: 18, justifyContent: "center"}}>
+                    <Text style={styles.text}>
                         Projectors: {roomInfo.find((elements => elements.room === modalRoom)).projectors}
                     </Text>
-                    <Text style={{fontSize: 18, justifyContent: "center"}}>
+                    <Text style={styles.text}>
                         Computers: {roomInfo.find((elements => elements.room === modalRoom)).computers}
                     </Text>
                 </View>
 
-                <View style={{ margin: 15, justifyContent: "center"}}>
-                    <Text style={{fontSize: 18}}>
+                <View style={styles.view}>
+
+                    <Text style={styles.text}>
                         Access: {roomInfo.find((elements => elements.room === modalRoom)).access}
                     </Text>
-                    <Text style={{fontSize: 18}}>
+
+
+                    <Text style={styles.text}>
                         Other: {roomInfo.find((elements => elements.room === modalRoom)).other}
                     </Text>
                 </View>
-                <Image style={{ height: imageHeight, width: imageWidth, resizeMode: "contain", alignSelf: "center", borderRadius: 5,margin: 5
-                    }} source={imagePath}/>
+                <Image style={{
+                    height: imageHeight,
+                    width: imageWidth,
+                    resizeMode: "contain",
+                    alignSelf: "center",
+                    borderRadius: 5,
+                    margin: 5
+                }} source={imagePath}/>
             </View>
 
 
@@ -58,6 +74,18 @@ const RoomScreen = ({ customStyle, modalRoom, setShowModal }) => {
 
 export default RoomScreen;
 
-//3210  1.jpg
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 18,
+        flex: 1,
+        textAlign: "center"
+    },
 
-//"../../assets/rooms/"+modalRoom.slice(0,1)+modalRoom.slice(2,5) +"  1.jpg"
+    view: {
+        margin: 15,
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+    },
+});
+

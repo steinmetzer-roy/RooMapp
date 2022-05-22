34a583c8b85dfa7b55c3895683fec336d5decda7
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-05-22 14:39:52 +0200

    Extend database config + tests

--- RooMapp/DBhelper.js
+++ RooMapp/DBhelper.js
@@ -1,13 +1,24 @@
-function ping_server() {
-    fetch("http://localhost:5005/", {
-        "method": "GET",
-        "headers": {}
-    }).then(response => {
-        if (response.status) return true
-    }).catch(err => {
-        return false;
-    });
-}
+let ping_status_promise = null
+
+function ping_server(){
+    return new Promise((resolve, reject) => {
+        fetch("http://localhost:5005/", {
+            "method": "GET",
+            "credentials": 'same-origin',
+            "headers": {
+                'Authorization': 'Basic ' + btoa('root:root')
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                //console.log(response.status);
+                resolve("Server is up");
+            }
+    
+        }).catch(err => {
+            reject("Server not reachable");
+        });
+    })
+} 
 
 
 function copy_db_entries() {
@@ -18,29 +29,30 @@ function copy_db_entries() {
     let promise_req
     let promise_json
 
-    promise_req = fetch("http://localhost:5005/test_database/_all_docs?include_docs=true", {
+    return new Promise((resolve) => {
+        promise_req = fetch("http://localhost:5005/test_database/_all_docs?include_docs=true", {
         "method": "GET",
         "credentials": 'same-origin',
         "headers": {
             'Authorization': 'Basic ' + btoa('root:root'),
         }
-    });
+    }).catch( console.log("Nope 1"));
 
     promise_req.then(_response => {
         response = _response;
         promise_json = response.json();
-
         promise_json.then(_response => {
             json_message = _response;
-            console.log(_response)
             for (let i = json_message["rows"].length - 1; i >= 0; i--) {
                 db_entries_json["entries"].push(json_message["rows"][i]['doc']);
             }
-            console.log(db_entries_json)
-        });
+            resolve(db_entries_json)
+        }).catch(reject("Nope 2"));
+        
+    }).catch(reject("Nope 3"));
+    })
 
-        if (_response.status) return true
-    }).catch(err => {
-        return false;
-    });
+    
 }
+
+export { ping_server, copy_db_entries }

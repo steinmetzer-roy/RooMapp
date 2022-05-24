6e603e4e7f328edc7b604e674c25a988370bc3a6
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-05-24 20:42:08 +0200

    Fix connection bug

--- RooMapp/DBhelper.js
+++ RooMapp/DBhelper.js
@@ -1,9 +1,9 @@
 let ping_status_promise = null
-let server = "1a6d-2a06-4944-16fe-3e00-50f8-bc85-faf7-4a9b.eu.ngrok.io"
+let server = "ec3e-2a06-4944-16fe-3e00-50f8-bc85-faf7-4a9b.eu.ngrok.io"
 
 function ping_server() {
     return new Promise((resolve, reject) => {
-        fetch(`http://${server}:5005/`, {
+        fetch(`https://${server}/`, {
             "method": "GET",
             "credentials": 'same-origin',
             "headers": {
@@ -31,7 +31,7 @@ function copy_db_entries() {
     let promise_json
 
     return new Promise((resolve,reject) => {
-        promise_req = fetch("http://100.64.220.151:5005/test_database/_all_docs?include_docs=true", {
+        promise_req = fetch(`https://${server}/test_database/_all_docs?include_docs=true`, {
             "method": "GET",
             "headers": {
               "Content-Type": "application/json"

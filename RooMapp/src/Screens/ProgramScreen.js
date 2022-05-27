27b4d9f3138216335d5015f0b2b0dc91a4f86cbe
steinmetzer-roy <roy.steinmetzer.001@student.uni.lu>
2022-05-27 16:01:01 +0200

    Add programm download from DB

--- RooMapp/src/Screens/ProgramScreen.js
+++ RooMapp/src/Screens/ProgramScreen.js
@@ -1,55 +1,71 @@
 import { View, Text, Pressable } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Modal } from 'react-native-web';
+import { copy_db_entries, ping_server } from '../../DBhelper';
 
 
 const SearchScreen = ({ customStyle }) => {
+
+    function updateOptions() {
+        ping_server().then(a => {
+            if (a === "Server is up") {
+                console.log("OK")
+                copy_db_entries().then(a => {
+                    for (let i = 0; i < a.entries.length; i++) {
+                        //console.log(a.entries[i])
+                        if (a.entries[i].program !== undefined) {
+                            setplans([...plans, a.entries[i]])
+                            //console.log("entered")
+                            for (let j = 0; j < a.entries[i].courses.length; j++) {
+                                //console.log(a.entries[i].courses[j])
+                                setmodalContent([...modalContent, <Text key={a.entries[i].courses[j].name} >{a.entries[i].courses[j].name}</Text>])
+                            }
+                        }
+                    }
+                    console.log(plans)
+                })
+            } else {
+                console.log("NOK")
+            }
+        }).catch(console.log("Couldn't connect to server"))
+    }
+
     const [plans, setplans] = useState([
         {
-            "Program": "BINFO 1th sem",
+            "program": "BINFO 1th sem",
             "likes": 1,
             "dislikes": 1,
-            "Courses": [
+            "courses": [
                 "Mathematic Discrete",
                 "Programming 1",
                 "Calculus",
                 "Technical English 1",
             ]
-        },
-        {
-            "Program": "BINFO 2th sem",
-            "likes": 1,
-            "dislikes": 1,
-            "Courses": []
-        },
-        {
-            "Program": "BINFO 3th sem",
-            "likes": 1,
-            "dislikes": 1,
-            "Courses": []
-        },
-        {
-            "Program": "BINFO 4th sem",
-            "likes": 1,
-            "dislikes": 1,
-            "Courses": []
-        },
+        }
     ]);
 
     const [list, setlist] = useState();
     const [modalContent, setmodalContent] = useState([])
     const [showModal, setshowModal] = useState(false)
+    const [loaded, setloaded] = useState(false)
+
+    //updateOptions();
+    if (!loaded) {
+        updateOptions();
+        setloaded(true);
+    }
 
     return (
+
         <View style={customStyle.container}>
             <View style={customStyle.courseWrapper}>
                 <View style={customStyle.titleWrapper}>
-                    <Text style={customStyle.title}>Search & download</Text>
+                    <Text style={customStyle.title}>Programs</Text>
                 </View>
 
                 <Modal visible={showModal} style={{ alignItems: "center", justifyContent: "center", }} animationType="slide" transparent={true}>
-                    <Pressable onPress={() => {setshowModal(false);setmodalContent([])}} style={{alignItems: "center", justifyContent: "center", flex: 1}}>
-                        <View style={{backgroundColor:"grey", padding:10, borderColor:"lightgrey", borderWidth:2 }}>
+                    <Pressable onPress={() => { setshowModal(false); setmodalContent([]) }} style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+                        <View style={{ backgroundColor: "grey", padding: 10, borderColor: "lightgrey", borderWidth: 2 }}>
                             {modalContent}
                         </View>
                     </Pressable>
@@ -58,15 +74,23 @@ const SearchScreen = ({ customStyle }) => {
                 <View>
                     {plans.map(element => {
                         return (
-                            <Pressable key={element.Program} onPress={() => {
-                                setmodalContent(
-                                    element.Courses.map(course => {
-                                        return ([...modalContent, <Text key={course} >{course}</Text>])
-                                    }))
-                                setshowModal(true)
+                            <Pressable key={element.program} onPress={() => {
+                                if (element.courses !== undefined) {
+                                    setmodalContent(
+                                        element.courses.map(course => {
+                                            let jsx = [];
+                                            if (course.name !== undefined) {
+                                                jsx = [...jsx, <Text key={course.name} >{course.name}</Text>]
+                                                return jsx;
+                                            }else{
+                                                return ([...modalContent, <Text key={course} >{course}</Text>])
+                                            }
+                                        }))
+                                    setshowModal(true)
+                                }
                             }}>
                                 <View style={customStyle.item}>
-                                    <Text>Likes:{element.likes} Dislikes:{element.dislikes} Program: {element.Program}</Text>
+                                    <Text>Likes: {element.likes} Dislikes: {element.dislikes} Program: {element.program}</Text>
                                 </View>
                             </Pressable>
                         )
